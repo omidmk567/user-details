027cd82a660111bd79cf1908e7c3d94e8930226a
Omid Mohkamkar <omidmk567@gmail.com>
2022-12-31 23:21:13 +0330

    Implement api for user favorite language.

--- resources/js/github-apis.js
+++ resources/js/github-apis.js
@@ -11,3 +11,31 @@ export async function getUserDetails(username) {
         return Promise.reject(err);
     });
 }
+
+export async function getUserFavoriteLanguage(username) {
+    return await octokit.request('GET /users/{username}/repos', {
+        username: username
+    })
+        .then(response => {
+            return Promise.resolve(response.data.sort((a, b) => {
+                return Date.parse(b['pushed_at']).valueOf() - Date.parse(a['pushed_at']).valueOf()
+            }).slice(0, 5));
+        })
+        .catch(Promise.reject)
+        .then(repositories => {
+            return Promise.all(repositories.map(repo => octokit.request(`GET ${repo['languages_url']}`).then(value => value.data)))
+        })
+        .catch(Promise.reject)
+        .then(languages => {
+            let maxKey, maxValue = 0;
+            languages.forEach(lang => {
+                Object.entries(lang).forEach(([key, value]) => {
+                    if (value > maxValue) {
+                        maxKey = key;
+                        maxValue = value;
+                    }
+                });
+            });
+            return Promise.resolve(maxKey);
+        })
+}
