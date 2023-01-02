b9e9a7d863d78be390792ab88c701943b280171c
Omid Mohkamkar <omidmk567@gmail.com>
2023-01-03 00:48:17 +0330

    Dont retry on requests.

--- resources/js/github-apis.js
+++ resources/js/github-apis.js
@@ -1,6 +1,12 @@
 import {Octokit} from "https://cdn.skypack.dev/octokit?dts";
 
-const octokit = new Octokit({auth: `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`});
+const octokit = new Octokit({
+    auth: `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`, request: {
+        timeout: 0
+    }, retry: {
+        enabled: false
+    }
+});
 
 export async function getUserDetails(username) {
     return await octokit.request('GET /users/{username}', {
