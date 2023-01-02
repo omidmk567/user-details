e0374f90e75716dddb3196b6ffdb2960ab6519bb
Omid Mohkamkar <omidmk567@gmail.com>
2023-01-03 00:53:30 +0330

    Simple script to call apis

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,37 +1,59 @@
-import {getUserDetails} from "./github-apis.js";
+import {getUserDetails, getUserFavoriteLanguage} from "./github-apis.js";
+
+const requiredFields = ['avatar_url', 'name', 'company', 'blog', 'location', 'email', 'bio']
 
 document.forms['user-input'].addEventListener('submit', fetchUserData)
 
 function fetchUserData(e) {
     e.preventDefault();
     clearNotification();
-    const username = new FormData(e.target).get('github_username');
+    const username = new FormData(e.target).get('github_username').toString();
     if (localStorage[username]) {
-        updateFields(localStorage[username])
+        updateUserDetailField(localStorage[username]);
     } else {
-        updateNotification('loading');
-        getUserDetails(username)
-            .then(JSON.stringify)
-            .then(details => {
-                updateFields(details)
-                return details;
-            })
-            .then(details => {
+        updateNotification('loading...');
+        getCompleteUserInfo(username)
+            .then(userInfo => {
                 clearNotification();
-                return details;
+                return userInfo;
+            })
+            .then(JSON.stringify)
+            .then(userInfo => {
+                updateUserDetailField(userInfo);
+                return userInfo;
+            })
+            .then(userInfo => {
+                localStorage.setItem(username, userInfo);
+                return userInfo;
             })
             .catch(err => {
-                updateNotification(err);
-                updateFields('');
+                if (err.toString() === 'HttpError: Not Found') {
+                    console.log('not found at all!');
+                    updateUserDetailField('username not found!');
+                } else {
+                    updateNotification(err);
+                    updateUserDetailField('error occured!');
+                }
                 return err;
-            })
-            .then(details => {
-                localStorage.setItem(username, details)
-            })
+            });
     }
 }
 
-function updateFields(details) {
+function getCompleteUserInfo(username) {
+    return getUserDetails(username)
+        .then(response => JSON.stringify(response, requiredFields))
+        .then(details => {
+            return getUserFavoriteLanguage(username)
+                .then(response => {
+                    const user_info = JSON.parse(details);
+                    user_info['fav_lang'] = response;
+                    return user_info;
+                })
+                .catch(err => new Error(err))
+        })
+}
+
+function updateUserDetailField(details) {
     document.getElementById('user-details').innerHTML = details;
 }
 
